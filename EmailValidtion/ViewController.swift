edd3298c4993e8c0792d420934e82255e3b2efac
Afreen <afreen.shaik@amzur.com>
2021-11-24 11:14:30 +0530

    Email validation

--- EmailValidtion/ViewController.swift
+++ EmailValidtion/ViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import Mailvalidator
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let res = Validator.validEmail("afrin@gmail.com")
+        print(res)
         // Do any additional setup after loading the view.
     }
 
