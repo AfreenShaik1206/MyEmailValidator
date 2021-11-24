de969967466d3489a29bbcd7659f2b957d68dcce
Afreen <afreen.shaik@amzur.com>
2021-11-24 13:45:07 +0530

    validation

---
+++ EmailValidtion/Mailvalidator.framework/Headers/Mailvalidator.h
@@ -0,0 +1,18 @@
+//
+//  Mailvalidator.h
+//  Mailvalidator
+//
+//  Created by Amzur on 19/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mailvalidator.
+FOUNDATION_EXPORT double MailvalidatorVersionNumber;
+
+//! Project version string for Mailvalidator.
+FOUNDATION_EXPORT const unsigned char MailvalidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mailvalidator/PublicHeader.h>
+
+
